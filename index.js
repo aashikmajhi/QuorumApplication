64a7740be9139cb214f52218173d2457db7c6c51
aashikmajhi <aashikmajhi@gmail.com>
2020-06-14 09:08:54 +0545

    minor change in index.js

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app = express();
+const questionRouter = require('./routes/questionRouter');
 
-app.get('/', (req,res) => {
-    res.send('Welcome to my app');
+const categoryRouter = require('./routes/categoryRouter');
+
+const userRouter = require('./routes/userRouter');
+
+const auth = require('./routes/auth');
+
+const app = express(); //instantiating express
+mongoose.connect(process.env.DbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+})
+    .then(() => { console.log('Database server Connected'); })
+    .catch((err) => { console.log(err); });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //reading data sent by client to the server
+app.get('/', (req, res, next) => {
+    res.send('Welcome! To my app');
+});
+
+app.use('/api/users', userRouter);
+app.use('/api/questions', auth.verifyUser, questionRouter);
+app.use('/api/categories', auth.verifyUser, categoryRouter);
+
+app.use((req, res, next) => {
+    let err = new Error('Not Found!');
+    err.status = 404;
+    next(err);
 })
 
-app.listen(3000, () => {
-    console.log('Server is running at localhost:3000');
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500);
+    res.json({
+        status: 'error',
+        message: err.message
+    })
 })
+
+app.listen(process.env.Port, () => {
+    console.log(`Server is running at localhost: ${process.env.Port}`);
+});
+
+module.exports = questionRouter;
